e1b8f68bfeb3cdeaad1f698e2b2cc791410262a9
isarikaya <isarikayaa@outlook.com>
2023-12-13 10:52:41 +0300

    update

--- index.js
+++ index.js
@@ -65,151 +65,208 @@ function convertJSONToTSV(jsonArr, tsvFilePath) {
   });
 }
 
-(async () => {
-  await createDir()
-  const users = await User.Get()
-  const base = "https://www.imdb.com/name/"
-  const browser = await puppeteer.launch({headless: "new", args: ['--disable-features=site-per-process']}) //headless old
-  const page = await browser.newPage()
-  const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
-  await page.setUserAgent(ua);
+const scrape = async(user, browser) => {
+  try {
+    await createDir()
+    const base = "https://www.imdb.com/name/"
+    const page = await browser.newPage()
+    const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
+    await page.setUserAgent(ua);
 
-  const getGender = async() => {
-    return await page.evaluate(() => {
-      return document.querySelector('.sc-7c4234bd-0 ul[role="presentation"] li[role="presentation"]')?.innerText.toLowerCase()
-    })
-  }
-  const loadMore = async () => {
-    const gender = await getGender()
-    const selector = `[data-testid='Filmography'] #${gender}-previous-projects button[data-testid='nm-flmg-paginated-all-${gender}']`
-    const loadMoreButtonExists = await page.evaluate(selector => {
-      const loadMoreButton = document.querySelector(selector)
-      if (loadMoreButton) {
-          loadMoreButton.click()
-          return true
+    const getGender = async () => {
+      try {
+        await page.waitForSelector('[data-testid="hero__pageTitle"]');
+        
+        const gender = await page.evaluate(() => {
+          const rows = document.querySelector('[data-testid="hero__pageTitle"]').parentElement.querySelectorAll('ul[role="presentation"] li[role="presentation"]');
+          
+          for (const row of rows) {
+            if (row.innerText === "Actor" || row.innerText === "Actress") {
+              return row.innerText.toLowerCase();
+            }
+          }
+        });
+        return gender;
+      } catch (error) {}
+    }
+    const loadMore = async () => {
+      const gender = await getGender()
+      const selector = `[data-testid='Filmography'] #${gender}-previous-projects button[data-testid='nm-flmg-paginated-all-${gender}']`
+      const loadMoreButtonExists = await page.evaluate(selector => {
+        const loadMoreButton = document.querySelector(selector)
+        if (loadMoreButton) {
+            loadMoreButton.click()
+            return true
+        }
+        return false
+      }, selector)
+      if (loadMoreButtonExists) {
+          await new Promise(r => setTimeout(r, 500))
+          await loadMore()
       }
-      return false
-    }, selector)
-    if (loadMoreButtonExists) {
-        await new Promise(r => setTimeout(r, 500))
-        await loadMore()
     }
-  }
-  const seeAllEpisode = async() => {
-    const seeAllButton = await page.$("[data-testid='promptable'] .ipc-see-more__text")
-    if (seeAllButton) {
-      await seeAllButton.click()
-      await new Promise(r => setTimeout(r, 250))
-      await seeAllEpisode()
+    const seeAllEpisode = async() => {
+      const seeAllButton = await page.$("[data-testid='promptable'] .ipc-see-more__text")
+      if (seeAllButton) {
+        await seeAllButton.click()
+        await new Promise(r => setTimeout(r, 250))
+        await seeAllEpisode()
+      }
     }
-  }
-  const getEpisodeData = async() => {
-    let episodes = []
-    episodes = await page.evaluate(() => {
-      const regex = /\(\d{4}\)/gm
-      const items = Array.from(document.querySelectorAll('[data-testid="promptable"] .ipc-promptable-base__content a.episodic-credits-bottomsheet__menu-item'))
-      return items.map(item => {
-        const p = item.querySelector("p.sc-d77789e-1")
-        const season_episode = p.querySelector("ul > li:first-child")?.innerText || ""
-        const match = p.querySelector("ul > li:last-child")?.innerText.match(regex)
-        const title_year = match ? match[0] : "";
-        const character = item.querySelector(".sc-d77789e-3.wrap-content")?.innerText || ""
-        return { season_episode, title_year, character}
+    const getEpisodeData = async() => {
+      let episodes = []
+      episodes = await page.evaluate(() => {
+        const regex = /\(\d{4}\)/gm
+        const items = Array.from(document.querySelectorAll('[data-testid="promptable"] .ipc-promptable-base__content a.episodic-credits-bottomsheet__menu-item'))
+        return items.map(item => {
+          const p = item.querySelector("p.sc-d77789e-1")
+          const season_episode = p.querySelector("ul > li:first-child")?.innerText || ""
+          const match = p.querySelector("ul > li:last-child")?.innerText.match(regex)
+          const title_year = match ? match[0] : "";
+          const character = item.querySelector(".sc-d77789e-3.wrap-content")?.innerText || ""
+          return { season_episode, title_year, character}
+        })
       })
-    })
-    return episodes
-  }
-  const getEpisodes = async() => {
-    let result = []
-    const tabs = await page.$$('[data-testid="episodic-navigation-container"] div > ul li.ipc-tab')
-    if(tabs && tabs.length > 0) {
-      for(const tab of tabs) {
-        await tab.click()
-        await new Promise(r => setTimeout(r, 1500))
+      return episodes
+    }
+    const getEpisodes = async() => {
+      let result = []
+      const tabs = await page.$$('[data-testid="episodic-navigation-container"] div > ul li.ipc-tab')
+      if(tabs && tabs.length > 0) {
+        for(const tab of tabs) {
+          await tab.click()
+          await new Promise(r => setTimeout(r, 1500))
+          await seeAllEpisode()
+          await new Promise(r => setTimeout(r, 1500))
+          const episodes = await getEpisodeData()
+          result = [...result, ...episodes]
+        }
+      } else {
         await seeAllEpisode()
         await new Promise(r => setTimeout(r, 1500))
-        const episodes = await getEpisodeData()
-        result = [...result, ...episodes]
+        result = await getEpisodeData()
       }
-    } else {
-      await seeAllEpisode()
-      await new Promise(r => setTimeout(r, 1500))
-      result = await getEpisodeData()
+      return result
     }
-    return result
-  }
-  const getPopupData = async() => {
-    const data = {}
-    const isEpisodes = await page.$('[data-testid="promptable"] .sc-688347b3-0 > a.character-summary-episodic-credit > ul')
-    data.title = await page.evaluate(() => {
-      return document.querySelector('[data-testid="promptable"] .sc-a78ec4e3-2 .ipc-title a')?.innerText || ""
-    })
-    data.item_code = await page.evaluate(() => {
-      const regex = /tt(\d+)/;
-      const itemCodeEl = document.querySelector('[data-testid="promptable"] .sc-a78ec4e3-2 .ipc-title a')
-      const match = itemCodeEl?.getAttribute("href")?.match(regex)
-      return match ? match[0] : ""
-    })
-    if(isEpisodes) {
-      await isEpisodes.click()
-      await new Promise(r => setTimeout(r, 1500))
-      data.lastResult = await getEpisodes()
-    } else {
-      data.title_year = await page.evaluate(() => {
-        return document.querySelector('[data-testid="promptable"] .sc-a78ec4e3-2 > ul > li')?.innerText || ""
+    const getPopupData = async() => {
+      const data = {}
+      const selector = '[data-testid="promptable"] .sc-688347b3-0 > a.character-summary-episodic-credit > ul'
+      const isEpisodesExists = await page.evaluate((sel) => {
+        return document.querySelector(sel) !== null;
+      }, selector);
+      data.title = await page.evaluate(() => {
+        return document.querySelector('[data-testid="promptable"] .sc-a78ec4e3-2 .ipc-title a')?.innerText || ""
       })
-      data.character = await page.evaluate(() => {
-        return document.querySelector('[data-testid="promptable"] .ipc-expandableSection > span > ul')?.innerText || ""
+      data.item_code = await page.evaluate(() => {
+        const regex = /tt(\d+)/;
+        const itemCodeEl = document.querySelector('[data-testid="promptable"] .sc-a78ec4e3-2 .ipc-title a')
+        const match = itemCodeEl?.getAttribute("href")?.match(regex)
+        return match ? match[0] : ""
       })
-    }
-    return [data]
-  }
-  const previousJobs = async() => {
-    let previousJobs = []
-    const gender = await getGender()
-    const infoButtons = await page.$$(`[data-testid='Filmography'] #${gender}-previous-projects #accordion-item-${gender}-previous-projects .ipc-accordion__item__content_inner > ul > li > button`)
-    if(infoButtons && infoButtons.length > 0) {
-      for(const btn of infoButtons) {
-        await btn.click()
+      if(isEpisodesExists) {
+        await page.click(selector);
         await new Promise(r => setTimeout(r, 1500))
-        let data = await getPopupData()
-        previousJobs = [...previousJobs, ...data]
-        await page.keyboard.press('Escape')
-        await new Promise(r => setTimeout(r, 500))
+        data.lastResult = await getEpisodes()
+      } else {
+        data.title_year = await page.evaluate(() => {
+          return document.querySelector('[data-testid="promptable"] .sc-a78ec4e3-2 > ul > li')?.innerText || ""
+        })
+        data.character = await page.evaluate(() => {
+          return document.querySelector('[data-testid="promptable"] .ipc-expandableSection > span > ul')?.innerText || ""
+        })
+      }
+      return [data]
+    }
+    const previousJobs = async() => {
+      let previousJobs = []
+      const gender = await getGender()
+      const infoButtons = await page.$$(`[data-testid='Filmography'] #${gender}-previous-projects #accordion-item-${gender}-previous-projects .ipc-accordion__item__content_inner > ul > li > button`)
+      if(infoButtons && infoButtons.length > 0) {
+        for(const btn of infoButtons) {
+          await page.evaluate((btn) => btn.click(), btn);
+          await new Promise(r => setTimeout(r, 1500))
+          let data = await getPopupData()
+          previousJobs = [...previousJobs, ...data]
+          await page.keyboard.press('Escape')
+          await new Promise(r => setTimeout(r, 500))
+        }
+      }
+      return previousJobs
+    }
+
+    try {
+      let data = []
+      if(user && user.ID) {
+        console.log("scraping =>", user.ID)
+        await page.goto(base + user.ID, {waitUntil: "domcontentloaded"})
+        const basicUserData = await page.evaluate(() => {
+          const fullName = document.querySelector('[data-testid="hero__pageTitle"]')?.innerText ?? ""
+          const born = document.querySelectorAll('[data-testid="birth-and-death-birthdate"] span')[1]?.innerText || ""
+          const bornYear = +born.split(",")[1]?.trim() || "-"
+          return {fullName, bornYear}
+        })
+        const fullGender = await getGender()
+        if(fullGender === "actor") {
+          basicUserData.gender = "M"
+        } else if(fullGender === "actress") {
+          basicUserData.gender = "F"
+        } else {
+          basicUserData.gender = ""
+        }
+        basicUserData.userCode = user.ID
+        data.push(basicUserData)
+        await loadMore()
+        let prevs = await previousJobs()
+        const merged = prevs.map(item => {
+          const newItem = { ...basicUserData, ...item }
+          if (newItem.lastResult) {
+            newItem.lastResult = newItem.lastResult.map(lastResultItem => {
+              return newItem.lastResult = { ...basicUserData, ...lastResultItem };
+            });
+          }
+          return newItem
+        });
+        if(merged && merged.length > 0) {
+          convertJSONToTSV(merged, "./output/" + user.ID + ".tsv")
+        }
+        await User.Update(user.ID)
+        await isDone()
+        await page.close()
+      }
+    } catch(err) {
+      if (err.name === 'TimeoutError') {
+        console.log('Navigation timeout exceeded. Continue...');
+        await page.close()
+      } else {
+        await page.close()
+        return { error: err.message }
       }
     }
-    return previousJobs
-  }
+  } catch(err) {console.log(err)}
+}
 
-  for await (const user of users) {
-    let data = []
-    if(user && user.ID) {
-      console.log("scraping =>", user.ID)
-      await page.goto(base + user.ID)
-      const basicUserData = await page.evaluate(() => {
-        const fullName = document.querySelector('[data-testid="hero__pageTitle"]')?.innerText ?? ""
-        const born = document.querySelectorAll('[data-testid="birth-and-death-birthdate"] span')[1]?.innerText || ""
-        const bornYear = +born.split(",")[1]?.trim() || "-"
-        const gender = document.querySelector('.sc-7c4234bd-0 ul[role="presentation"] li[role="presentation"]')?.innerText === "Actor" ? "M" : "F" || ""
-        return {fullName, bornYear, gender}
-      })
-      basicUserData.userCode = user.ID
-      data.push(basicUserData)
-      await loadMore()
-      let prevs = await previousJobs()
-      const merged = prevs.map(item => {
-        const newItem = { ...basicUserData, ...item }
-        if (newItem.lastResult) {
-          newItem.lastResult = newItem.lastResult.map(lastResultItem => {
-            return newItem.lastResult = { ...basicUserData, ...lastResultItem };
-          });
-        }
-        return newItem
-      });
-      convertJSONToTSV(merged, "./output/" + user.ID + ".tsv")
-      await User.Update(user.ID)
-      await isDone()
-    }
+function chunkArray(array, size) {
+  const chunkedArr = []
+  for (let i = 0; i < array.length; i += size) {
+    chunkedArr.push(array.slice(i, i + size))
+  }
+  return chunkedArr
+}
+
+async function processChunk(chunk, browser) {
+  await Promise.all(chunk.map(async (users) => {
+    await scrape(users, browser);
+  }));
+}
+async function scrapeUsers() {
+  await createDir()
+  const browser = await puppeteer.launch({headless: "new", args: ['--disable-features=site-per-process']})
+  const users = await User.Get()
+  const chunks = chunkArray(users, 5)
+  for (const chunk of chunks) {
+    await processChunk(chunk, browser)
   }
   await browser.close()
-})()
+}
+
+scrapeUsers()
